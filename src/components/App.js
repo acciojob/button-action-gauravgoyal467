77948befa306c3f55e396c773750d1068dc5860e
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-17 07:41:54 +0000

    1st try

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+const[visible,setVisible]=useState(false);
   return (
     <div className="App" id="main">
       // Do not alter the main div
+      <p id="para" className={visible ? "show":"hide"}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={()=>setVisible(!visible)}>Click Me</button>
     </div>
   );
 }
